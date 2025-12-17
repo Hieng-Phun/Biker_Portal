dde00f93862a7306bbeb97038f90eb752d337977
Hieng <hiengphun168@gmail.com>
2025-12-17 11:05:29 +0700

    update setting

--- biker/settings.py
+++ biker/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 LOGIN_URL = '/admin/login/' # Change this to your custom login URL if not using admin
